51ff9ba5e502b206c20929896ad29b3736d4a0de
Miroslav <lemmas.online@gmail.com>
2017-10-18 15:40:19 +0300

    small fix

--- src/views/chat.blade.php
+++ src/views/chat.blade.php
@@ -12,6 +12,7 @@
                     <div class="modal-body">Вы точно хотите удалить данный тикет?</div>
                     <div class="modal-footer">
                         {{ method_field('DELETE') }}
+                        {{ csrf_field() }}
                         <input type="hidden" name="id" value="">
                         <button type="submit" class="btn btn-danger">Удалить</button>
                     </div>
